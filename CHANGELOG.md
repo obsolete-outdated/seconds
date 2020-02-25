2d84b2c653c95cf1c84420b63ee6af02f053847b
Aleksei <54249358+Moonspeaker@users.noreply.github.com>
2020-02-25 07:32:17 +0300

    updated to use utc package

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,17 @@
+## [1.0.5] - Minor
+
+fixed dependency version
+
+## [1.0.4] - added datetime_utc_converter dependency
+
+now handles conversion to UTC timezone
+
 ## [1.0.4] - Reversed function
 
 added dateTimeUTCFromSeconds
 
 ## [1.0.2] - Minor
+
 package info maintaining
 
 ## [1.0.0] - Initial release
--- lib/seconds.dart
+++ lib/seconds.dart
@@ -1,9 +1,11 @@
 library seconds;
 
+import 'package:datetime_utc_converter/datetime_utc_converter.dart';
+
 const int millisInSecond = 1000;
 
 int get secondsNowUTC => secondsFromDateTimeUTC(DateTime.now());
 
-int secondsFromDateTimeUTC(final DateTime dateTime) => dateTime.toUtc().millisecondsSinceEpoch ~/ millisInSecond;
+int secondsFromDateTimeUTC(final DateTime dateTime) => dateTimeConvertedToUTC(dateTime).millisecondsSinceEpoch ~/ millisInSecond;
 
 DateTime dateTimeUTCFromSeconds(final int seconds) => DateTime.fromMillisecondsSinceEpoch(seconds * millisInSecond, isUtc: true);
